a95597dad73627b00ff16788e07ff6e5325ca11e
stutibimali <71383846+stutibimali@users.noreply.github.com>
2020-12-04 19:37:24 +0530

    Update preparing.py

--- preparing.py
+++ preparing.py
@@ -7,4 +7,4 @@ X_train, X_test, y_train, y_test = train_test_split(X,y,
                                         test_size=0.2, random_state=0)
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
-LinearRegression()
+
