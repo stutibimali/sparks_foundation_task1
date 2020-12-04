ab49c31ca14e8d2d09e8ff79ecfd3ccfeb722bd4
stutibimali <71383846+stutibimali@users.noreply.github.com>
2020-12-04 19:30:04 +0530

    Create preparing.py

---
+++ preparing.py
@@ -0,0 +1,10 @@
+#Preparing the Data
+X = s_data.iloc[:, :-1].values
+y = s_data.iloc[:, 1].values
+X = s_data.iloc[:, :-1].values
+y = s_data.iloc[:, 1].values
+X_train, X_test, y_train, y_test = train_test_split(X,y,
+                                        test_size=0.2, random_state=0)
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+LinearRegression()
