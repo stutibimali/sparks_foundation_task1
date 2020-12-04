4da77041df7857ec5ad82944866b1326b44311e0
stutibimali <71383846+stutibimali@users.noreply.github.com>
2020-12-04 19:33:23 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,3 +1,12 @@
 #Making Prediction
 print(X_test)
 y_pred = regressor.predict(X_test)
+
+com_df = pd.DataFrame({"Actual" : y_test,"Predicted" : y_pred}) 
+
+com_df
+
+plt.xlabel("Hours Studied")
+plt.ylabel("Percentage of Marks")
+plt.scatter(s_data.Hours,s_data.Scores,color='red',marker='*')
+plt.plot(s_data.Hours,regressor.predict(s_data[['Hours']]), color='green')
